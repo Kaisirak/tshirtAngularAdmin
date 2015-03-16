d7b8e7c9bac81ceca61289752354132efbef5471
Jules Girodon <julesgirodon@Juless-iMac.local>
2015-03-16 13:34:02 -0700

    border edition 3

--- scripts/app.directives.js
+++ scripts/app.directives.js
@@ -254,9 +254,8 @@ angular.module("app.directives", [])
 							//		canvas.setOverlayImage('../img/overlay.png', canvas.renderAll.bind(canvas));		// Preload Both Overlay Images
 							//	});
 
-              var myrecti = new fabric.Rect({ top: 100, left: 150, width: 200, height: 400, selectable: false, fill: 'transparent', strokeWidth: 1, stroke: '#333' })
-              canvas.add(myrecti);
-              canvas.renderAll();
+              var myBorder = new fabric.Rect({ top: 100, left: 160, width: 200, height: 400, selectable: false, fill: 'transparent', strokeWidth: 1, stroke: '#333' })
+              canvas.add(myBorder);
 
 							$("#onlinedesigner").on('click', '#crtBtn', function() {
 								var addedTxt = new fabric.IText('edit text', {
