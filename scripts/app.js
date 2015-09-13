1c415938104ef45501edf78624134524d768bf2c
Ernstberger <thiebaud.ernstberger@gmail.com>
2015-09-13 14:36:55 -0700

    :D

--- scripts/app.js
+++ scripts/app.js
@@ -578,7 +578,7 @@ function($rootScope, $scope, $location, $http, $rootScope, $route, $cookieStore,
           console.log(data);
           
           angular.forEach(myThis.available_vneck_colors, function(color, key) {
-            console.log(color);
+            //console.log(color);
             myThis.HexToName[color.id] = color.name;
           });
           console.log(myThis.HexToName);
